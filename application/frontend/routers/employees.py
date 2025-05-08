a4d2a16fe279bb6f217b8822b330872e34ecc3d5
NovSergey <serge2017.sergey@yandex.ru>
2025-05-08 11:58:02 +0300

    generating qrcodes

--- application/frontend/routers/employees.py
+++ application/frontend/routers/employees.py
@@ -1,11 +1,16 @@
+import asyncio
+import io
 import uuid
+from urllib.parse import quote
 
+import qrcode
 from fastapi import APIRouter, Request
 from fastapi import Depends
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import StreamingResponse
 
 from application.backend.api.employees.dependencies import employee_by_id
-from application.backend.api.employees.schemas import Employee, EmployeeBase
+from application.backend.api.employees.schemas import Employee
 from application.backend.api.groups.crud import get_groups
 from application.backend.api.objects.crud import get_objects
 from application.backend.api.users.dependencies import jwt_required_redirect
@@ -46,4 +51,21 @@ async def get_employee_page(request: Request, session = Depends(db_helper.sessio
     })
 
 
+@router.get("/employee/{employee_id}/qr", name="download_qr")
+async def download_qr(employee: Employee = Depends(employee_by_id)):
+    loop = asyncio.get_event_loop()
+    buf = await loop.run_in_executor(None, generate_qr_image, employee.id)
+    filename_utf8 = f"{employee.full_name}.png"
+    filename_quoted = quote(filename_utf8)
 
+    return StreamingResponse(buf, media_type="image/png", headers={
+        "Content-Disposition": f"attachment; filename*=UTF-8''{filename_quoted}"
+    })
+
+
+def generate_qr_image(employee_id: uuid.UUID) -> io.BytesIO:
+    img = qrcode.make(str(employee_id))
+    buf = io.BytesIO()
+    img.save(buf, format="PNG")
+    buf.seek(0)
+    return buf
